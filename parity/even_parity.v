8b80012789e17255e9d77e9146ebe55bba2f6893
Prashant Pandey <prashantpandeyiuet@gmail.com>
2026-03-16 19:17:46 +0530

    vector notation

--- parity/even_parity.v
+++ parity/even_parity.v
@@ -1,3 +1,3 @@
-module even_parity_generator(input [3:0] data, output parity);
+module even_parity_generator(input [3:0] data, output parity); //[3:0] makes vectors: d0, d1, d2, d3
   assign parity = ^data; //xor of the data 
 endmodule
